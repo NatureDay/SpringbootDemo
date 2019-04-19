c8be670c0b982c24fe6d5667e099626f8c9c7313
Nature_Day <1004934782@qq.com>
2019-04-19 19:47:04 +0800

    update project

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -2,7 +2,11 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.User;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/hello")
@@ -11,6 +15,13 @@ public class HelloController {
     @RequestMapping
     public String hello() {
         return "Hello Spring-Boot";
+    }
+
+    @RequestMapping("/info")
+    public Map<String, String> getInfo(@RequestParam String name) {
+        Map<String, String> result = new HashMap<>();
+        result.put("name", name);
+        return result;
     }
 
     @RequestMapping("/getUser")

05aaeb4b4f49c58f7c66a5fdea68301c76a5337b
Nature_Day <1004934782@qq.com>
2019-04-25 19:46:55 +0800

    update project

--- src/main/java/com/example/demo/configurer/WebMvcConfig.java
+++ src/main/java/com/example/demo/configurer/WebMvcConfig.java
@@ -3,10 +3,10 @@ package com.example.demo.configurer;
 import com.example.demo.interceptor.TestInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-public class MyWebAppConfigurer extends WebMvcConfigurerAdapter {
+public class WebMvcConfig extends WebMvcConfigurationSupport {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {

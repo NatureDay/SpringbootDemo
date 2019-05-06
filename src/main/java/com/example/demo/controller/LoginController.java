d17934137c5aa8b98b1b53ff07b57db3dc8d7ca4
Nature_Day <1004934782@qq.com>
2019-05-06 19:17:51 +0800

    update project

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.base.CommonException;
 import com.example.demo.base.Result;
 import com.example.demo.model.User;
 import com.example.demo.util.ResultUtil;
+import com.google.common.base.Strings;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +21,14 @@ public class LoginController {
 
     @PostMapping
     public Result<User> login(@RequestBody Map<String, Object> params) {
+        String userName = (String) params.get("userName");
+        String password = (String) params.get("password");
+        if (Strings.isNullOrEmpty(userName) || Strings.isNullOrEmpty(password))
+            throw CommonException.create(111, "用户名密码不能为空");
 
+        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(userName, password);
+        Subject subject = SecurityUtils.getSubject();
+        subject.login(usernamePasswordToken);
 
         return ResultUtil.success(null);
     }
